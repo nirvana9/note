03a1978668d3871aa9a43857e48b60a1080ae8e1
name <email>
2015-02-05 15:26:38 +0800

    --------

--- c++/redis.cpp
+++ c++/redis.cpp
@@ -8,14 +8,14 @@
 
 int main()
 {
-  Redis *r = new Redis();
-  if(!r->connect("192.168.13.128", 6379))
-  {
-    printf("connect error!\n");
-    return 0;
-  }
-  r->set("name", "Mayuyu");
-  printf("Get the name is %s\n", r->get("name").c_str());
-  delete r;
-  return 0;
+	Redis *r = new Redis();
+	if(!r->connect("127.0.0.1", 50002))
+	{
+		printf("connect error!\n");
+		return 0;
+	}
+	r->set("name", "Mayuyu");
+	printf("Get the name is %s\n", r->get("name").c_str());
+	delete r;
+	return 0;
 }
--- c++/redis.h
+++ c++/redis.h
@@ -16,45 +16,46 @@
 
 class Redis
 {
-public:
+	public:
+		Redis(){}
+		
+		~Redis()
+		{
+			this->_connect = NULL;
+			this->_reply = NULL;	    	    
+		}
+		
+		bool connect(std::string host, int port)
+		{
+			this->_connect = redisConnect(host.c_str(), port);
+			if(this->_connect != NULL && this->_connect->err)
+			{
+				printf("connect error: %s\n", this->_connect->errstr);
+				return 0;
+			}
+			return 1;
+		}
+		
+		std::string get(std::string key)
+		{
+			this->_reply = (redisReply*)redisCommand(this->_connect, "GET %s", key.c_str());
+			std::string str = this->_reply->str;
+			freeReplyObject(this->_reply);
+			return str;
+		}
+		
+		void set(std::string key, std::string value)
+		{
+			void *a=redisCommand(this->_connect, "SET %s %s", key.c_str(), value.c_str());
+			if (a != NULL)
+			{
+				std::cout << *(int *)a << std::endl;
+			}
+		}
+	private:
+		redisContext* _connect;
+		redisReply* _reply;
 
-    Redis(){}
-
-    ~Redis()
-  {
-  this->_connect = NULL;
-    this->_reply = NULL;	    	    
-  }
-
-  bool connect(std::string host, int port)
-  {
-      this->_connect = redisConnect(host.c_str(), port);
-    if(this->_connect != NULL && this->_connect->err)
-    {
-        printf("connect error: %s\n", this->_connect->errstr);
-      return 0;
-    }
-    return 1;
-  }
-
-    std::string get(std::string key)
-  {
-    this->_reply = (redisReply*)redisCommand(this->_connect, "GET %s", key.c_str());
-    std::string str = this->_reply->str;
-    freeReplyObject(this->_reply);
-    return str;
-  }
-
-  void set(std::string key, std::string value)
-  {
-  redisCommand(this->_connect, "SET %s %s", key.c_str(), value.c_str());
-  }
-
-private:
-
-    redisContext* _connect;
-  redisReply* _reply;
-        
 };
 
 #endif  //_REDIS_H_
