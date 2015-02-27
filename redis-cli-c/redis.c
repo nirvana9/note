5142d813546bfc2ecccdd2d4cce3ed023bc7133c
name <email>
2015-02-27 10:20:55 +0800

    as

--- redis-cli-c/redis.c
+++ redis-cli-c/redis.c
@@ -22,55 +22,18 @@ int main(int argc, char **argv) {
         }
         exit(1);
     }
-
-    /* PING server */
-    reply = redisCommand(c,"PING");
-    printf("PING: %s\n", reply->str);
+    reply = redisCommand(c,"LLEN mylist");
+	printf("INCR counter: %lld\n", reply->integer);
     freeReplyObject(reply);
-
-    /* Set a key */
-    reply = redisCommand(c,"SET %s %s", "foo", "hello world");
-    printf("SET: %s\n", reply->str);
-    freeReplyObject(reply);
-
-    /* Set a key using binary safe API */
-    reply = redisCommand(c,"SET %b %b", "bar", (size_t) 3, "hello", (size_t) 5);
-    printf("SET (binary API): %s\n", reply->str);
-    freeReplyObject(reply);
-
-    /* Try a GET and two INCR */
-    reply = redisCommand(c,"GET foo");
-    printf("GET foo: %s\n", reply->str);
-    freeReplyObject(reply);
-
-    reply = redisCommand(c,"INCR counter");
-    printf("INCR counter: %lld\n", reply->integer);
-    freeReplyObject(reply);
-    /* again ... */
-    reply = redisCommand(c,"INCR counter");
-    printf("INCR counter: %lld\n", reply->integer);
-    freeReplyObject(reply);
-
-    /* Create a list of numbers, from 0 to 9 */
-    reply = redisCommand(c,"DEL mylist");
-    freeReplyObject(reply);
-    for (j = 0; j < 10; j++) {
-        char buf[64];
-
-        snprintf(buf,64,"%d",j);
-        reply = redisCommand(c,"LPUSH mylist element-%s", buf);
+	
+//    for (j = 0; j < 10; j++) {
+//        char buf[64];
+//        snprintf(buf,64,"%d",j);
+        reply = redisCommand(c,"RPOP mylist");
+		printf("----%s\n",reply->str);
+		printf("I: %lld\n", reply->integer);
         freeReplyObject(reply);
-    }
-
-    /* Let's check what we have inside the list */
-    reply = redisCommand(c,"LRANGE mylist 0 -1");
-    if (reply->type == REDIS_REPLY_ARRAY) {
-        for (j = 0; j < reply->elements; j++) {
-            printf("%u) %s\n", j, reply->element[j]->str);
-        }
-    }
-    freeReplyObject(reply);
-
+//    }
     /* Disconnects and frees the context */
     redisFree(c);
 
